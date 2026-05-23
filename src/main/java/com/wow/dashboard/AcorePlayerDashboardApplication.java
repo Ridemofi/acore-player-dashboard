fc782d89dde453f179fac197ef82f781aba7a6a4
Ridemofi <ridemofi2006@gmail.com>
2026-05-23 11:34:07 -0500

    Initial commit

---
+++ src/main/java/com/wow/dashboard/AcorePlayerDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.wow.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcorePlayerDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcorePlayerDashboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wow/dashboard/AcorePlayerDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wow.dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcorePlayerDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
